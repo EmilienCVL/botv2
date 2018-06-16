36d4014890edb2fbb3145cbd7ea08b9105641fd6
DraxiioEG <39008957+DraxiioEG@users.noreply.github.com>
2018-06-17 00:30:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ var bot = new Discord.Client();
 var prefix = ("!");
 
 bot.on('ready', () => {
-    bot.user.setPresence({ game: {name: 'ses victimes agoniser..', type: 2} });
-    console.log("SCP- ███ opérationnel");
+    bot.user.setPresence({ game: {name: 'tirer sur des bouteilles', type: 2} });
+    console.log("Pew Pew opérationnel");
 });
 
 bot.login(process.env.TOKEN);
 
 bot.on("guildMemberAdd", member => {
-    let role = member.guild.roles.find("name", "Class-D");
-    member.guild.channels.find("name", "welcome").send(`:spy:Le SCP- ${member.user.username} vient d'être capturé par la Fondation.`)
+    let role = member.guild.roles.find("name", "Tueurs Novices");
+    member.guild.channels.find("name", "nouveaux-arrivants").send(`OH LES MAINS PEAU DE LAPIN {member.user.username}EST LA :gun:`)
     member.addRole(role)
 })
 
 bot.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "welcome").send(`:spy:Le SCP- ${member.user.username} vient d'être tué par la Fondation.`)
+    member.guild.channels.find("name", "nouveaux-arrivants").send(`Oups, balle perdue dans la tête de {member.user.username} Adieu :skull:`)
 });
