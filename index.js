eb14c2a825701dc73e0dde3f637b812e3d85cc99
DraxiioEG <39008957+DraxiioEG@users.noreply.github.com>
2018-07-10 21:14:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -12,8 +12,8 @@ bot.login(process.env.TOKEN);
 
 bot.on("guildMemberAdd", member => {
     let role = member.guild.roles.find("name", "MiniKiwi");
-    member.guild.channels.find("name", "bienvenue").send(`Heeey ! Bienvenue à ${member.user.username} sur le Discord du Kiwi
+    member.guild.channels.find("name", "bienvenue").send(`**Heeey ! Bienvenue à ${member.user.username} sur le Discord du Kiwi
 
-    Le canal #reglement est ton ami ! Amuse-toi bien ici :wink: ! `)
+    Le canal #reglement est ton ami ! Amuse-toi bien ici :wink: !** `)
     member.addRole(role)
 });
