a46042155b81b44f8cc9e461bc6dadd06d604519
DraxiioEG <39008957+DraxiioEG@users.noreply.github.com>
2018-07-10 20:32:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,18 +4,16 @@ var bot = new Discord.Client();
 var prefix = ("!");
 
 bot.on('ready', () => {
-    bot.user.setPresence({ game: {name: 'tirer sur des bouteilles', type: 1} });
-    console.log("Pew Pew opérationnel");
+    bot.user.setPresence({ game: {name: 'interviewer un Kiwi', type: 2} });
+    console.log("Carte Kiwi opérationnelle");
 });
 
 bot.login(process.env.TOKEN);
 
 bot.on("guildMemberAdd", member => {
-    let role = member.guild.roles.find("name", "Tueurs Novices");
-    member.guild.channels.find("name", "nouveaux-arrivants").send(`HAUT LES MAINS PEAU DE LAPIN ${member.user.username} EST LA :gun:`)
-    member.addRole(role)
-})
+    let role = member.guild.roles.find("name", "MiniKiwi");
+    member.guild.channels.find("name", "welcome").send(`Heeey ! Bienvenue à ${member.user.username} sur le Discord du Kiwi
 
-bot.on("guildMemberRemove", member => {
-    member.guild.channels.find("name", "nouveaux-arrivants").send(`Oups, balle perdue dans la tête de ${member.user.username} Adieu :skull:`)
+    Le canal #reglement est ton ami ! Amuse-toi bien ici :wink: ! `)
+    member.addRole(role)
 });
