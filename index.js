417d453f1c389705e85230ed8185ec71308c9e90
EmilienCVL <36304372+EmilienCVL@users.noreply.github.com>
2018-08-29 14:26:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -124,14 +124,14 @@ mbot.on('message', data => {
 
 :page_with_curl: Quelques points importants à respecter pour une bonne entente ;
 
-Restez respectueux et polis avec tout le monde.
+``Restez respectueux et polis avec tout le monde.
 Toute forme de discrimination est strictement interdite.
 Merci de bien vouloir rester impartial avec tous les serveurs présents sur ce Discord.
-Les pseudonymes et les noms de jeux inappropriés ainsi que l'usurpation d'identité est strictement interdit.
+Les pseudonymes et les noms de jeux inappropriés ainsi que l'usurpation d'identité est strictement interdit.``
 
 :book: Au niveau des discussions, tout ce qui est compris ici sera sanctionné ;
 
-Les sujets / contenus pédophiles, pornographiques, religieux ou de propagande sont interdits.
+``Les sujets / contenus pédophiles, pornographiques, religieux ou de propagande sont interdits.
 Tout spam abusif (y compris le spam de mentions), troll, spoil ou provocation.
 La diffusion d'informations privées, personnelles ou pas est strictement interdite.
 La publicité en toute forme est autorisée seulement dans le salon textuel #spam-pub.
@@ -139,12 +139,11 @@ Le NSFW dans sa globalité est prohibée.
 Le spam auditif, les screamers audios ainsi que le troll sont strictement interdits.
 Le changement répétitif de channel est interdit.
 Les soundboards et modification de voix sont prohibés.
-Votre pseudonyme doit être facilement mentionnable sur le serveur.
+Votre pseudonyme doit être facilement mentionnable sur le serveur.``
 
-Au moment où vous rejoignez ce Discord, vous acceptez automatiquement de respecter ce règlement.
-`);
+Au moment où vous rejoignez ce Discord, vous acceptez automatiquement de respecter ce règlement.`);
   }
-});
+
     
     if (message.content === prefix + "roles"){
         var roles_embed = new Discord.RichEmbed()
