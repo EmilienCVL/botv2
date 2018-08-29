d3968ed34273650d1ddd90253f9673797350a51c
EmilienCVL <36304372+EmilienCVL@users.noreply.github.com>
2018-08-29 04:38:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var prefix = ("!");
 bot.login(process.env.TOKEN);
 
 bot.on('ready', () => {
-    bot.user.setPresence({ game: { name: '!infos • Private Game', type: 0} });
+    bot.user.setPresence({ game: { name: 'être dev par Draxiio', type: 0} });
     console.log("Taupe'Info ✔");
 });
 
